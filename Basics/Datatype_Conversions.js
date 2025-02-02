e9936853c30fe716eb2f2903d6d002c9411dd55f
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2025-02-03 00:46:19 +0530

    Conversions of DataTypes

---
+++ Basics/Datatype_Conversions.js
@@ -0,0 +1,44 @@
+let score = "sami"
+
+// console.log(typeof score);  
+// console.log(typeof (score));
+
+// let valueInNumber = Number(scope);
+// console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+/*
+//'33' => 33
+// '33abc' => NaN // but type is number
+//true => 1
+//false => 0
+//null => 0
+//undefined => NaN
+//'' => 0
+*/
+
+let isLoggedin  =  true;
+
+let booleanIsLoggedin = Boolean(isLoggedin);
+
+// console.log(booleanIsLoggedin);
+
+/*
+// 0 => false
+1 => true
+-1 => true
+'' => false
+' ' => true
+'sami' => true
+*/
+
+
+let someNumber = 33
+
+let StringNumber = String(someNumber);
+
+console.log(typeof StringNumber)
+console.log(StringNumber);
+
+
