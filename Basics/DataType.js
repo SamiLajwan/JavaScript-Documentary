7dc822a398b84977f867bfd46f65ae4459298363
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2025-02-03 00:23:05 +0530

    Open

---
+++ Basics/DataType.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS codes as Newer version of JS
+
+// alert("Hello World"); // we are using node js not browser
+ 
+//code readability should be high
+// console.log(3+3); 
+
+let name = "sami" // string
+let age = 18  // number
+let isLoggedin = false // boolean
+let state;
+
+
+//primitive data types-->
+//number range => 2^53 -1 to -2^53 -1
+
+// string range => " " or ' ' or ` ` 
+
+//boolean  => true or false
+
+//null => standalone value / empty value
+
+//undefined => variable declared but not assigned
+
+//symbol => unique value
+
+//BigInt => large number
+
+
+
+//Object => key value pair
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
