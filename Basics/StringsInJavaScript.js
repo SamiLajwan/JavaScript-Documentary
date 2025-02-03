9d61edca9ae2d55e7765573f3a67530a54bfdba2
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2025-02-03 14:38:10 +0530

    Strings

---
+++ Basics/StringsInJavaScript.js
@@ -0,0 +1,46 @@
+const name = "sami"
+// declaration of string using new keyword
+const gameNew = 'Abdul Sami Lajwan' 
+
+
+const repoCount = 50
+
+// console.log(name + repoCount + "values");
+
+// String interPolation
+// console.log(`Hello my name is ${gameNew} and my repoCount is ${repoCount} values`);
+
+//  console.log(gameNew[2]);
+ 
+//  console.log(gameNew.__proto__)  //declare object
+
+//  console.log(gameNew.length) // length of string
+
+//  console.log(gameNew.toUpperCase())     // convert string to uppercase
+
+// console.log(gameNew.charAt(6)); // return character at index 2
+
+// console.log(gameNew.indexOf('Abdul'));      // return index of character
+
+// const newString= gameNew.substring(0,4) // return substring from index 0 to 4
+// console.log(newString);
+
+// const anotherString = gameNew.slice(0, 4); // return substring from index -1 to 4
+// console.log(anotherString);
+
+// const newStringOne = "   sami   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());  // remove white spaces from string
+
+
+// const url = "https://www.google.com/sami%20lajwan"
+// console.log(url.replace('%20', '-')); // replace %20 with - in string
+// console.log(url.includes('google')); // check if string includes google - true
+
+
+//  const newString = gameNew.split(' ') // split string into array
+//  console.log(newString);
+
+
+
+
