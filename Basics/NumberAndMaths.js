05dbf8bf2577f390871a0bdf0ee71635f068f216
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2025-02-03 15:08:50 +0530

    Basics Maths

---
+++ Basics/NumberAndMaths.js
@@ -0,0 +1,47 @@
+// // const score = 400
+// // console.log(score); //400
+
+// const balance = new Number(100)
+// console.log(balance); //[Number: 100]
+
+// console.log(balance.toString().length); //3
+
+// console.log(balance.toFixed(2)); //100.00
+
+// const otherNum = 123.7890
+// console.log(otherNum.toPrecision(5)); //123.79
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  //10,00,000  en-IN is for India
+
+//*********** Maths ************//
+
+// console.log(Math.PI) //3.141592653589793
+
+// console.log(Math.abs(-4)) //4
+
+// console.log(Math.round(4.7)) //5
+
+// console.log(Math.ceil(4.1)) //5
+
+// console.log(Math.floor(4.9)) //4
+
+// console.log(Math.sqrt(16)) //4
+
+// console.log(Math.min(4,5,6,7,8,.00001)) //0.00001
+
+console.log(Math.random()); // 0 to 1
+console.log((Math.random()*10) +  1); // 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
