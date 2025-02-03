ea70c1646207db42c0e358dd79543fd0220c6a61
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2025-02-03 09:59:27 +0530

    Open

--- Basics/Datatype_Conversions.js
+++ Basics/Datatype_Conversions.js
@@ -38,7 +38,53 @@ let someNumber = 33
 
 let StringNumber = String(someNumber);
 
-console.log(typeof StringNumber)
-console.log(StringNumber);
+// console.log(typeof StringNumber)
+// console.log(StringNumber);
+
+
+// ****************** OPerations ******************
+
+
+let value = 3
+let negVAlue = -value
+
+// console.log(negVAlue);
+
+// console.log(2+2); 
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "sami"
+let str2 = "khan"
+
+let str3 = str1 + " " + str2
+// console.log(str3);
+
+// console.log("1"+2); // 12
+// console.log(1+"2"); // 12
+// console.log("1"+2+3); // 123
+// console.log(1+2+"2");  // 32
+
+// console.log(+true); //1
+// console.log(+false); //0
+
+let num1, num2, num3
+
+num1 = num2= num3 = 2+2 
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
